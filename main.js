cd05530dac65e702a7bc1d3c16f4aab054ed9f81
jonathan03062011 <76205715+jonathan03062011@users.noreply.github.com>
2021-05-13 16:45:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function preload()
+{
+ mustache_filter=loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
+function setup()
+{
+ canvas=createCanvas(300,300);
+ canvas.center();
+ video=createCapture(VIDEO);
+ video.size(300,300);
+ video.hide();
+ posenet=ml5.poseNet(video,modelloaded);
+ posenet.on('pose',gotposes);
+}
+
+function modelloaded()
+{
+    console.log("model is loaded");
+
+}
+ var mustachex="";
+ var mustachey="";
+function gotposes(results)
+{
+ if(results.length>0)
+ {
+     console.log(results);
+     mustachex=results[0].pose.mustache.x;
+     mustachey=results[0].pose.mustache.y;
+ }
+}
+
+function draw()
+{
+ image(video,0,0,300,300);
+ image(mustache_filter,mustachex,mustachey,30,30);
+}
+
+function take_snapshot()
+{
+    save('jonathan.png');
+}
